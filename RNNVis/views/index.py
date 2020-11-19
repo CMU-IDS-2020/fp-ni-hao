76b0c1248aff49094566a7ca28d92870e17ee7a9
zshengli <shengliz@andrew.cmu.edu>
2020-11-18 22:58:33 -0500

    first draft: source of happiness frontend

--- RNNVis/views/index.py
+++ RNNVis/views/index.py
@@ -47,4 +47,8 @@ def show_index():
         # return flask.redirect(flask.url_for('login'))
         context['logname'] = 'none'
         pass
+    context['sentence'] = 'Someone on a forum out there suggested that the reason the trailing space is added to the highlighted word is because Microsoft Windows assumes you’re going to be pasting that word somewhere else in your paragraph and that you’ll likely need that space.'
+    context['word_weight'] = url_for('uploaded_file', filename='affection_sample.png')
+    context['pred_prob'] = url_for('uploaded_file', filename='prediction_prob.png')
+    context['explanation'] = "Above is the visualization of the model's output. Based on the sentence, the model has categorized its happy source as xxx. "
     return flask.render_template("index.html", **context)
