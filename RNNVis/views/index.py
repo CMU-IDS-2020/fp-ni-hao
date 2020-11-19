b50abef6dd2e620001cb8fc958377236e72933ae
tuliyun <tuliyun@umich.edu>
2020-11-19 16:42:00 -0500

    added lime plots + sentence switch

--- RNNVis/views/index.py
+++ RNNVis/views/index.py
@@ -5,6 +5,7 @@ URLs include:
 /
 """
 import os
+import string
 import shutil
 import tempfile
 import uuid
@@ -51,25 +52,27 @@ def show_index():
     """Display / route."""
     context = {} 
     labels = ['achievement','affection','bonding','enjoy the moment','exercise','leisure','nature']
-    labeldict = {'achievement': 0,'affection': 1,'bonding': 2,'enjoy the moment': 3,'exercise': 4, 'leisure': 5,'nature': 6}
+    labeldict = {'achievement': 0,'affection': 1,'bonding': 2,'enjoy_the_moment': 3,'exercise': 4, 'leisure': 5,'nature': 6}
     global lr_model
     global lr_vectorizer
     global dataset
-    random.seed(10)
+    # Comment the line to fix the sentence content
+    # random.seed(10)
     index = random.randint(0,14124)
-    sentence = [dataset["cleaned_hm"][index]]
+    sentence = [dataset["cleaned_hm"].str.lower()[index]]
     pred = lr_model.predict(lr_vectorizer.transform(sentence))
     c = make_pipeline(lr_vectorizer, lr_model)
     explainer = LimeTextExplainer(class_names=labels)
     exp = explainer.explain_instance(sentence[0], c.predict_proba, labels=[0,1,2,3,4,5,6])
-    word_weight = exp.as_list(label=labeldict[pred[0]])
-    pred_prob = lr_model.predict_proba(lr_vectorizer.transform(sentence))[0]
-    pred_prob = [(labels[i], pred_prob[i]) for i in range(7)]
-    pred_prob = sorted(pred_prob, key = lambda x: x[1], reverse = True)
-    context['weight_test'] = word_weight
-    context['prob_test'] = pred_prob
-    context['sentence'] = sentence[0]
-    context['word_weight'] = url_for('uploaded_file', filename='affection_sample.png')
-    context['pred_prob'] = url_for('uploaded_file', filename='prediction_prob.png')
+    exp.save_to_file('RNNVis/static/model/lime.html', text=sentence[0],labels=(labeldict[pred[0]],))
+    # word_weight = exp.as_list(label=labeldict[pred[0]])
+    # pred_prob = lr_model.predict_proba(lr_vectorizer.transform(sentence))[0]
+    # pred_prob = [(labels[i], pred_prob[i]) for i in range(7)]
+    # pred_prob = sorted(pred_prob, key = lambda x: x[1], reverse = True)
+    # context['weight_test'] = word_weight
+    # context['prob_test'] = pred_prob
+    context['sentence'] = dataset["cleaned_hm"][index]
+    # context['word_weight'] = url_for('uploaded_file', filename='affection_sample.png')
+    # context['pred_prob'] = url_for('uploaded_file', filename='prediction_prob.png')
     context['explanation'] = "Above is the visualization of the model's output. Based on the sentence, the model has categorized its happy source as "+ str(pred[0])+". "
     return flask.render_template("index.html", **context)
